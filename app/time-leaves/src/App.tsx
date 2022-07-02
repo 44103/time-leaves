bc88bf6bdadbb656faa32badf0981b9193a40cd4
kenko <universeengineer@gmail.com>
2022-07-02 11:29:48 +0900

    :sparkles: create record component

--- app/time-leaves/src/App.tsx
+++ app/time-leaves/src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Record from "./components/record";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>State</th>
+            <th align='left'>Date</th>
+            <th align='left'>Event</th>
+          </tr>
+        </thead>
+        <tbody>
+          <Record />
+          <Record />
+          <Record />
+        </tbody>
+      </table>
     </div>
   );
 }
---
+++ app/time-leaves/src/components/record.tsx
@@ -0,0 +1,12 @@
+
+const Record = () => {
+  return (
+    <tr>
+      <td>ToDo</td>
+      <td>2000/01/01 12:00:00</td>
+      <td>Event</td>
+    </tr>
+  );
+}
+
+export default Record;
